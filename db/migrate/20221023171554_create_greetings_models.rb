349557528a304f1edb87e31f800f9c667dab41e3
joffreynk <joyjoffrey@gmail.com>
2022-10-23 19:26:20 +0200

    update model and add seeds data to use for testing

--- db/migrate/20221023171554_create_greetings_models.rb
+++ db/migrate/20221023171554_create_greetings_models.rb
@@ -2,6 +2,7 @@ class CreateGreetingsModels < ActiveRecord::Migration[7.0]
   def change
     create_table :greetings_models do |t|
       t.string :message
+      t.string :author
 
       t.timestamps
     end
